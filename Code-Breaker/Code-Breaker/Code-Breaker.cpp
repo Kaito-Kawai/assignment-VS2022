4833f656c4c6b2af86b45a9f6a3d3a5728fd3bcd
Kaito-Kawai <ktc25a32a0004@edu.kyoto-tech.ac.jp>
2026-06-27 15:43:21 +0900

    完成！
    -- Debug
    デバッグ用コード
    Debug --

--- Code-Breaker/Code-Breaker/Code-Breaker.cpp
+++ Code-Breaker/Code-Breaker/Code-Breaker.cpp
@@ -3,6 +3,8 @@
 #include <array>
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
+#include <limits>
 
 struct JudgeResult
 {
@@ -25,6 +27,7 @@ public:
 	void generateNumbers()
 	{
 		numbers.clear();
+
 		for (int i = 0; i < 4; i++)
 		{
 			numbers.push_back(dist(gen));
@@ -119,7 +122,7 @@ public:
 			result.blow += std::min(randomCount[i], inputCount[i]);
 		}
 
-		result.clear = result.hit = 4;
+		result.clear = result.hit == 4;
 	}
 
 	const JudgeResult& getResult()const
@@ -130,10 +133,92 @@ public:
 
 class Output
 {
+public:
+	void showInputGuide(int challengeCount) const
+	{
+		std::cout << "Challenge " << challengeCount << std::endl;
+		std::cout << "Input 4 numbers from 0 to 9." << std::endl;
+		std::cout << std::endl;
+	}
 
+	void showResult(const JudgeResult& result) const
+	{
+		std::cout << "Hit: " << result.hit << std::endl;
+		std::cout << "Blow: " << result.blow << std::endl;
+		std::cout << std::endl;
+	}
+
+	void showClear(int challengeCount)const
+	{
+		std::cout << "Game Clear!!!" << std::endl;
+		std::cout << "Challenge Count: " << challengeCount << std::endl;
+	}
 };
 
 int main()
 {
+	RandomHolder randomHolder;
+	Output output;
 
+	randomHolder.generateNumbers();
+
+	int challengeCount = 0;
+	bool isClear = false;
+
+	while (!isClear)
+	{
+		InputHolder inputHolder;
+		challengeCount++;
+
+		output.showInputGuide(challengeCount);
+
+		//	Debug --
+		//const std::vector<int>& debugNumbers = randomHolder.getNumbers();
+
+		//std::cout << "Random: ";
+
+		//for (int number : debugNumbers)
+		//{
+		//	std::cout << number << " ";
+		//}
+
+		//std::cout << std::endl;
+		//	-- Debug
+
+		for (int i = 0; i < 4; i++)
+		{
+			int inputValue = 0;
+
+			std::cout << "Number" << i + 1 << ": ";
+			std::cin >> inputValue;
+
+			if (std::cin.fail())
+			{
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout << "Please input number." << std::endl;
+				i--;
+				continue;
+			}
+
+			try
+			{
+				inputHolder.addValue(inputValue);
+			}
+			catch (const std::exception& e)
+			{
+				std::cout << e.what() << std::endl;
+				i--;
+			}
+		}
+
+		JudgeHolder judgeHolder(&randomHolder, &inputHolder);
+		judgeHolder.judge();
+
+		const JudgeResult& result = judgeHolder.getResult();
+		output.showResult(result);
+
+		isClear = result.clear;
+	}
+	output.showClear(challengeCount);
 }
